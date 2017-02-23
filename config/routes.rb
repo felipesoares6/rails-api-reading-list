7953c07a02f21b4c69ce9b50b8365361bf7108f4
Felipe Soares <felipeluizsoares@gmail.com>
2017-02-23 13:30:02 -0300

    Add POST method

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :books, only: :index
+  resources :books
   resources :finished_books
   resources :genres
 end
--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -1,14 +1,14 @@
-require "test_helper"
-require "minitest/pride"
+require 'test_helper'
+require 'minitest/pride'
 
 class ListingBooksTest < ActionDispatch::IntegrationTest
   setup do
-    Book.create!(title: "Pragmatic Programmer", rating: 5)
-    Book.create!(title: "Enders Game", rating: 4)
+    Book.create!(title: 'Pragmatic Programmer', rating: 5)
+    Book.create!(title: 'Enders Game', rating: 4)
   end
 
-  test "listing books" do
-    get "/books"
+  test 'listing books' do
+    get '/books'
 
     assert_equal 200, response.status
     assert_equal Mime[:json], response.content_type
@@ -16,7 +16,7 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
     assert_equal Book.count, json(response.body).size
   end
 
-  test "listing top rated books" do
+  test 'listing top rated books' do
     get '/books?rating=5'
 
     assert_equal 200, response.status
